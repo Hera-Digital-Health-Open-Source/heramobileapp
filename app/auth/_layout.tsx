926917691c12a2fe651a7efbbc5040f540baf2cc
Husam Burhan <husam.burhan@gmail.com>
2025-01-26 18:08:37 +0300

    adding stack

---
+++ app/auth/_layout.tsx
@@ -0,0 +1,10 @@
+import { Stack } from "expo-router";
+
+export default function AuthLayout({children}: {children: React.ReactNode}){
+  return (
+    <Stack screenOptions={{headerShown: false}}>
+      <Stack.Screen name="login" />
+      <Stack.Screen name="otp-screen" />
+    </Stack>  
+  );
+}
