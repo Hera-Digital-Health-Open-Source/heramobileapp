3e9b97678b74e921f233c4e0fcf5457d14069b93
Husam Burhan <husam.burhan@gmail.com>
2025-01-26 18:10:12 +0300

    adding new providres

--- context/AppProvider.tsx
+++ context/AppProvider.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import { AuthProvider } from './AuthContext';
 import HttpClientProvider from './HttpClientContext';
+import { RegistrationProvider } from './RegistrationContext';
+import { PregnancyProvider } from './PregnancyContext';
+import { LoadingProvider } from './LoadingContext';
+
 
 export const AppProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <HttpClientProvider>
-      <AuthProvider>{children}</AuthProvider>
-    </HttpClientProvider>
+    <LoadingProvider>
+      <HttpClientProvider>
+        <AuthProvider>
+          <RegistrationProvider>
+            <PregnancyProvider>
+              {children}
+            </PregnancyProvider>
+          </RegistrationProvider>
+        </AuthProvider>
+      </HttpClientProvider>
+    </LoadingProvider>
   );
 };
