0d01daa9eaea3e8cff48ada8ea5ef4f2f88fdf23
Husam Burhan <husam.burhan@gmail.com>
2025-01-26 18:07:30 +0300

    creating a dedicated loading provider and use it inside LoadingModel

---
+++ context/LoadingContext.tsx
@@ -0,0 +1,30 @@
+import React, { createContext, useState, useContext, ReactNode } from 'react';
+
+interface LoadingContextProps {
+  isLoading: boolean;
+  setLoading: (loading: boolean) => void;
+}
+
+const LoadingContext = createContext<LoadingContextProps | undefined>(undefined);
+
+interface RegistrationProviderProps {
+  children: ReactNode;
+}
+
+export const  LoadingProvider = ({ children }: RegistrationProviderProps) => {
+  const [isLoading, setLoading] = useState(false);
+
+  return (
+    <LoadingContext.Provider value={{ isLoading, setLoading }}>
+      {children}
+    </LoadingContext.Provider>
+  );
+};
+
+export const useLoading = (): LoadingContextProps => {
+  const context = useContext(LoadingContext);
+  if (!context) {
+    throw new Error('useLoading must be used within a LoadingProvider');
+  }
+  return context;
+};
