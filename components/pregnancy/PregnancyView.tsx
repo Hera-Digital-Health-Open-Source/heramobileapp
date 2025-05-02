2d72ca80897b42b1093041d386b1762b17d24b09
Husam Burhan <husam.burhan@gmail.com>
2025-05-02 07:32:21 +0300

    new component

---
+++ components/pregnancy/PregnancyView.tsx
@@ -0,0 +1,207 @@
+import { SafeAreaView, View , Text, StyleSheet} from "react-native";
+import { GlobalStyles, Spacing } from "@/assets/theme";
+import Button, { ButtonStyles } from "@/components/Button";
+import { useEffect, useState } from "react";
+import DateModalPicker from "@/components/DateModalPicker";
+import DropDownPicker from "@/components/DropDownPicker";
+import { router } from "expo-router";
+import { RequestConfig, useHttpClient } from "@/context/HttpClientContext";
+import { useAuth } from "@/context/AuthContext";
+import IPregnancy from "@/models/IPregnancy";
+
+export default function PregnancyView({introduceText, pregnancy, isInRegistrationProcess} : {introduceText: string, pregnancy? : IPregnancy, isInRegistrationProcess: boolean}){
+  const [pregnancyCalculationMethod, setPregnancyCalculationMethod] = useState<'lastMenstrualDate' | 'pregnancyWeek' | undefined>(undefined);
+  const [lastMenstrualDate, setLastMenstrualDate] = useState<Date | undefined>(undefined);
+  const [prentalVisits, setPrentalVisits] = useState<string>("");
+  const [pregnancyWeek, setPregnancyWeek] = useState<string>("");
+  const {sendRequestFetch} = useHttpClient();
+  const {session} = useAuth();
+
+  const info = !introduceText ? `Alright! Let’s fill in the details and we will assist you by adding important doctor visit dates into “My Appointments”!` : introduceText;
+  const enableContinue = (pregnancyCalculationMethod === 'lastMenstrualDate' && lastMenstrualDate && prentalVisits && prentalVisits !== '-1') || 
+                          (pregnancyCalculationMethod === 'pregnancyWeek' && pregnancyWeek && pregnancyWeek !== '-1' && prentalVisits && prentalVisits !== '-1')
+  const pregnancyWeekItems = [{key: '-1', label: ''}, ...Array.from({ length: 42 }, (_, i) => i + 1).map(i => ({key: String(i), label: String(i)}))];
+
+  useEffect(() => {
+    if(pregnancy){
+      setLastMenstrualDate(!pregnancy.declared_pregnancy_week ? new Date(pregnancy.declared_date_of_last_menstrual_period) : undefined);
+      setPrentalVisits(pregnancy.declared_number_of_prenatal_visits);
+      setPregnancyWeek(pregnancy.declared_pregnancy_week);
+      setPregnancyCalculationMethod(pregnancy.declared_pregnancy_week ? 'pregnancyWeek' : 'lastMenstrualDate');
+    }
+  }, [pregnancy]);
+
+  const flipPregnancyCalculationMethod = (calculationMethod: 'lastMenstrualDate' | 'pregnancyWeek') => {
+    setPregnancyCalculationMethod(calculationMethod);
+  };
+
+  const addSavePregnancy = async () => {
+    let method: 'PATCH' | 'POST';
+    let url: string;
+    let requestConfig: RequestConfig;
+
+    if(pregnancy){
+      url = `/pregnancies/${pregnancy.id}/`;
+      method = 'PATCH'
+    } else {
+      url = `/pregnancies/`;
+      method = 'POST'
+    }
+  
+    const data = {
+      declared_pregnancy_week: !pregnancyWeek ? null : pregnancyWeek,
+      declared_date_of_last_menstrual_period: !pregnancyWeek ? lastMenstrualDate?.toISOString().split('T')[0] : null,
+      declared_number_of_prenatal_visits: prentalVisits,
+    };
+
+    requestConfig = {
+      url: url,
+      method: method,
+      data: data,
+      headers: {
+        'Accept-Language': 'en',
+        'Content-Type': 'application/json',
+        Authorization: 'Token ' + session
+      }
+    };
+
+    const response = await sendRequestFetch<null>(requestConfig);
+
+    if(response.error){
+      console.log(response.error)
+    } else {
+      if(pregnancy || !isInRegistrationProcess){
+        router.back();
+      } else {
+        router.push('/registration/pregnancy-info-complete')
+      }
+    }
+  }
+
+  return(
+    <SafeAreaView style={{flex:1, backgroundColor: '#fff'}}>
+      <View style={{flex: 1, gap: Spacing.large}}>
+        <View style={styles.container}>
+          <View style={{gap: Spacing.small}}>
+            <Text style={GlobalStyles.HeadingText}>Pregnancy</Text>
+            <Text style={GlobalStyles.NormalText}>{info}</Text>
+          </View>
+          <View style={{flex: 1}}>
+            <View style={{}}>
+              <Button
+                buttonType={ pregnancyCalculationMethod === 'lastMenstrualDate' ? ButtonStyles.FILLED : ButtonStyles.UNFILLED}
+                label="Enter Last Menstrual Date"
+                onPress={() => flipPregnancyCalculationMethod('lastMenstrualDate')}
+              />
+              <Button
+                buttonType={ pregnancyCalculationMethod === 'pregnancyWeek' ? ButtonStyles.FILLED : ButtonStyles.UNFILLED}
+                label="Enter Pregnancy Week"
+                onPress={() => flipPregnancyCalculationMethod('pregnancyWeek')}
+              />
+            </View>
+            <View style={{marginTop: Spacing.xlarge}}>
+              {pregnancyCalculationMethod === 'lastMenstrualDate' && (
+                <View style={{marginTop: Spacing.xlarge, gap: Spacing.xlarge}}>
+                  <View style={{gap: Spacing.medium}}>
+                    <Text style={GlobalStyles.NormalText}>Select Menstrual Period</Text>
+                    <DateModalPicker
+                      initialDate={lastMenstrualDate}
+                      onDateSelected={(date)=> {
+                        setLastMenstrualDate(date);
+                        setPregnancyWeek("");
+                      }} 
+                    />
+                  </View>
+                  <View style={{gap: Spacing.medium}}>
+                    <Text style={GlobalStyles.NormalText}>How many prenatal visits have you done?</Text>
+                    <DropDownPicker
+                      initialKeySelection={prentalVisits}
+                      items={[
+                        {key: '-1', label: ''},
+                        {key: '0', label: '0'},
+                        {key: '1', label: '1'},
+                        {key: '2', label: '2'},
+                        {key: '3', label: '3'},
+                        {key: '4', label: '4'}
+                      ]}
+                      onItemSelectionChanged={(key) => setPrentalVisits(key)}
+                    />
+                  </View>
+                </View>
+              )}
+              {pregnancyCalculationMethod === 'pregnancyWeek' && (
+                <View style={{marginTop: Spacing.xlarge, gap: Spacing.xlarge}}>
+                  <View style={{gap: Spacing.medium}}>
+                    <Text style={GlobalStyles.NormalText}>Select Pregnancy Week</Text>
+                    <DropDownPicker
+                      initialKeySelection={pregnancyWeek}
+                      items={pregnancyWeekItems}
+                      onItemSelectionChanged={(key) => {
+                        setPregnancyWeek(key);
+                        setLastMenstrualDate(undefined);
+                      }}
+                    />
+                  </View>
+                  <View style={{gap: Spacing.medium}}>
+                    <Text style={GlobalStyles.NormalText}>How many prenatal visits have you done?</Text>
+                    <DropDownPicker
+                      initialKeySelection={prentalVisits}
+                      items={[
+                        {key: '-1', label: ''},
+                        {key: '0', label: '0'},
+                        {key: '1', label: '1'},
+                        {key: '2', label: '2'},
+                        {key: '3', label: '3'},
+                        {key: '4', label: '4'}
+                      ]}
+                      onItemSelectionChanged={(key) => setPrentalVisits(key)}
+                    />
+                  </View>
+                </View>
+              )}
+            </View>
+          </View>
+          {(pregnancy || !isInRegistrationProcess) && (
+            <Button
+              buttonType={ enableContinue ? ButtonStyles.FILLED : ButtonStyles.DISABLED}
+              label={"Save"}
+              onPress={enableContinue ? addSavePregnancy : () => {}}
+            /> 
+          )}
+          {isInRegistrationProcess && (
+            <Button
+              buttonType={ enableContinue ? ButtonStyles.FILLED : ButtonStyles.DISABLED}
+              label={"Continue"}
+              onPress={enableContinue ? addSavePregnancy : () => {}}
+            /> 
+          )}
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: Spacing.large,
+    marginTop: Spacing.xxlarge,
+    gap: Spacing.xxlarge,
+    backgroundColor: '#fff',
+  },
+  continueButton: {
+    position: 'absolute',
+    bottom: 0,
+    left:Spacing.large,
+    right: Spacing.large
+  },
+  yesNoContainer: {
+    flex:1,
+    justifyContent: 'center',
+  },
+  yesNoButtonsContainer: {
+    flexDirection: 'row',
+    width: '100%',
+    gap: Spacing.xlarge
+  }
+});
