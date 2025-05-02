2a6ae6eca889e0f20963a086c3a8381ce1553e56
Husam Burhan <husam.burhan@gmail.com>
2025-05-02 07:32:30 +0300

    Pregnancy model

---
+++ models/IPregnancy.ts
@@ -0,0 +1,6 @@
+export default interface IPregnancy{
+  id: string;
+  declared_pregnancy_week: string;
+  declared_date_of_last_menstrual_period: string;
+  declared_number_of_prenatal_visits: string;
+}
