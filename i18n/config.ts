56d883c105d12f924f41ec2ce1ed075703379ccf
Husam Burhan <husam.burhan@gmail.com>
2025-05-08 06:48:57 +0300

    preparing Turkish translation file

--- i18n/config.ts
+++ i18n/config.ts
@@ -1,8 +1,9 @@
 import { I18n } from 'i18n-js';
 import en from './translations/en';
 import ar from './translations/ar';
+import tr from './translations/tr';
 
-const i18n = new I18n({ en, ar });
+const i18n = new I18n({ en, ar, tr });
 i18n.enableFallback = true;
 
 export default i18n;
