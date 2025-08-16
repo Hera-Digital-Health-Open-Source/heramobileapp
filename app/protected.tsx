b0081db4f6d4ca799cbc888efcb8b6eb87b24afc
Husam Burhan <husam.burhan@gmail.com>
2025-08-16 05:32:57 +0300

    applying zustand + cleaning code

--- app/protected.tsx
+++ app/protected.tsx
@@ -1,28 +1,29 @@
 // src/app/protected.tsx
 import React, { useEffect, useState } from 'react';
-import { useAuth } from '@/context/AuthContext';
+import { useAuthStore } from '@/store/authStore';
 import { Redirect } from 'expo-router';
 
 const Protected = ({ children }: { children: React.ReactNode }) => {
-  const { session, profile, preparingProfile, preparingSession } = useAuth();
+  const { session, userProfile } = useAuthStore();
   const [status, setStatus] = useState<'undetermined' | 'denied' | 'allowed'>('undetermined');
 
-  useEffect(() => {
-    if(!preparingProfile && !preparingSession){
-      if(session && profile){
-        setStatus('allowed');
-      } else {
-        setStatus('denied');
-      }
-    }
-  }, [preparingProfile, preparingSession]);
   // useEffect(() => {
-  //   if(session && profile){
-  //     setStatus('allowed');
-  //   } else {
-  //     setStatus('denied');
+  //   if(!preparingProfile && !preparingSession){
+  //     if(session && profile){
+  //       setStatus('allowed');
+  //     } else {
+  //       setStatus('denied');
+  //     }
   //   }
-  // }, [session, profile]);
+  // }, [preparingProfile, preparingSession]);
+
+  useEffect(() => {
+    if(session && userProfile){
+      setStatus('allowed');
+    } else {
+      setStatus('denied');
+    }
+  }, [session, userProfile]);
 
   if (status === 'denied') {
     return <Redirect href="/auth/login" />;
