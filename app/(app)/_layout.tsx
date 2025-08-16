b01d6cecd52ef80a1de14cb770092f161efb788c
Husam Burhan <husam.burhan@gmail.com>
2025-08-16 05:33:17 +0300

    cleaning code

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -5,28 +5,20 @@ import { Drawer } from 'expo-router/drawer';
 import CustomDrawerContent from '@/components/CustomDrawerContent';
 
 export default function AppLayout() {
-  // return (<Redirect href={'/doctor-appointment'} />);
   return (
-    // <Protected>
-    //   <Stack screenOptions={{headerShown: false}}>
-    //     <Stack.Screen name="index" options={{headerShown: false}} />
-    //   </Stack>
-    // </Protected>
     <Protected>
-    <GestureHandlerRootView style={{ flex: 1 }}>
-        <Drawer 
-          drawerContent={() => <CustomDrawerContent />}
-          screenOptions={{
-            headerShown: false,
-            drawerHideStatusBarOnOpen: true,
-            drawerActiveBackgroundColor: '#f00',
-            drawerActiveTintColor: '#000',
-            drawerLabelStyle: {marginLeft: -16},
-          }}
-        />
-          {/* <Drawer.Screen name='(mystacks)' />
-        </Drawer> */}
-    </GestureHandlerRootView>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+          <Drawer 
+            drawerContent={() => <CustomDrawerContent />}
+            screenOptions={{
+              headerShown: false,
+              drawerHideStatusBarOnOpen: true,
+              drawerActiveBackgroundColor: '#f00',
+              drawerActiveTintColor: '#000',
+              drawerLabelStyle: {marginLeft: -16},
+            }}
+          />
+      </GestureHandlerRootView>
     </Protected>
   );
 }
