8ba56959f70734f35f9befa474a7b836167f02d0
Husam Burhan <husam.burhan@gmail.com>
2025-08-18 06:38:22 +0300

    change the place + some changes

--- components/appointments-screen/MarkAsDoneButton.tsx
+++ components/appointments-screen/MarkAsDoneButton.tsx
@@ -1,5 +1,6 @@
-import { color, Spacing } from "@/assets/theme";
+import { Colors, Spacing } from "@/assets/theme";
 import { Pressable, Text, StyleProp, ViewStyle, StyleSheet } from "react-native";
+import { GlobalStyles } from "@/assets/theme";
 
 type Props = {
   label: string;
@@ -17,17 +18,19 @@ export default function MarkAsDoneButton({style, onPress, label}: Props){
 
 const styles = StyleSheet.create({
   button: {
+    // ...GlobalStyles.ButtonALT,
+    borderColor: Colors.primary, 
+    borderWidth: 1,
     height: 36,
     justifyContent: 'center',
-    borderColor: color.primary,
-    borderWidth: 1,
-    borderRadius: Spacing.medium
+    borderRadius: Spacing.medium,
+    minWidth: 110,
   },
   text: {
     fontWeight: "700",
     textAlign: 'center',
-    fontSize: 16,
+    fontSize: Spacing.large,
     fontFamily: 'Roboto-Bold',
-    color: color.primary,
+    color: Colors.primary,
   }
 })
