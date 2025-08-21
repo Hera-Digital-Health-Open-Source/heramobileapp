6fb417a2d2adc16acf024aba0a55fd0863dd4bf3
Husam Burhan <husam.burhan@gmail.com>
2025-08-21 08:57:48 +0300

    remove unsed code and comments

--- app/registration/(profile)/user-details.tsx
+++ app/registration/(profile)/user-details.tsx
@@ -1,32 +1,9 @@
-import { StyleSheet, SafeAreaView, Text, TextInput, View } from "react-native";
-import { GlobalStyles, Spacing } from "@/assets/theme";
-import GenderSwitcher from "@/components/GenderSwitcher";
-import { useState } from "react";
-import DateTimePickerModal from "react-native-modal-datetime-picker";
-import Button, { ButtonStyles } from "@/components/Button";
-import { router } from "expo-router";
+import { StyleSheet, SafeAreaView} from "react-native";
+import { Spacing } from "@/assets/theme";
 import { useRegistration } from "@/context/RegistrationContext";
 import ProfileView from "@/components/profile/ProfileView";
 
 export default function UserDetails(){
-  // const now = new Date();
-  // const date18YearsAgo = new Date();
-  // date18YearsAgo.setFullYear(now.getFullYear() - 18);
-
-  // const [selectedName, setSelectedName] = useState('');
-  // const [selectedDateOfBirth, setSelectedDateOfBirth] = useState(date18YearsAgo);
-  // const [showDatePicker, setShowDatePicker] = useState(false);
-  // const [selectedGender, setSelectedGender] = useState<'male' | 'female' | undefined>(undefined);
-  // const {setGender, setName, setDateOfBirth} = useRegistration();
-
-  // const enableContinue = selectedName.length > 0 && selectedGender !== undefined;
-
-  // const handleContinue = () => {
-  //   setGender(selectedGender);
-  //   setName(selectedName);
-  //   setDateOfBirth(selectedDateOfBirth);
-  //   router.push('/registration/terms-of-use');
-  // }
 
   return (
     <SafeAreaView style={{flex:1, backgroundColor: '#fff'}}>
