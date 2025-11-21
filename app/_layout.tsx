63d5e4b83809b4b2a08d6e64127b7f793875337f
Husam Burhan <husam.burhan@gmail.com>
2025-11-21 16:29:34 +0300

    Apply heap.io (#1)
    * Change the version number
    
    * Installing heap io
    
    * Config Heap and adding it to the app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,18 @@
 import LoadingModal from "@/components/LoadingModel";
 import { AppProvider } from "@/context/AppProvider";
 import { Slot } from "expo-router";
+import { useEffect } from 'react';
+import { Heap } from '@heap/heap-react-native-bridge';
+import { registerHeapAutocapture } from '@heap/heap-react-native-autocapture';
+
 
 export default function RootLayout() {
+  useEffect(() => {
+    Heap.logToConsole();                 // optional
+    Heap.startRecording("2800948075");  // required
+    registerHeapAutocapture(true);       // required
+  }, []);
+
   return (
     <AppProvider>
       <LoadingModal />
